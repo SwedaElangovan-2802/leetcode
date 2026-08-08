150fd309852f47e7cd00dce99479ba54a270cd00
SwedaElangovan-2802 <swedaelangovan28@gmail.com>
2026-08-08 21:14:55 +0530

    Time: 1 ms (30.06%), Space: 43.1 MB (52.67%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,13 +1,8 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        int l=s.length(),ans=0;
-        for(int i=l-1;i>=0;i--){
-            if(s.charAt(i)!=' '){
-                ans++;
-            }
-            else if(ans!=0){
-                break;
-            }
-        }return ans;
+        String[] t = s.split(" ");
+        int n = t.length;
+        return t[n-1].length();
+
     }
 }
